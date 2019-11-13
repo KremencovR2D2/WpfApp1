cd1791bfe13c2fd0e0c3b16874bb3d50e84fcf87
MitrixD <kremencov.r2d2@gmail.com>
2019-11-13 23:51:29 +0300

    Program interval array

--- DraftProject/DraftProject/Program.cs
+++ DraftProject/DraftProject/Program.cs
@@ -10,6 +10,41 @@ namespace DraftProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите начальное значение интервала:");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Введите конечное значение интервала:");
+            var b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Случайно сгенерированный массив чисел:");
+            int[] randomArray = new int[20];
+            List<int> itemsInRange = new List<int>();
+            List<int> itemsOutOfRange = new List<int>();
+            Random rand = new Random();
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = rand.Next(0, 100);
+                Console.Write(randomArray[i] + " ");
+            }
+            Console.WriteLine();
+            foreach (var item in randomArray)
+            {
+                if (item > a && item < b)
+                {
+                    itemsInRange.Add(item);
+                }
+                else
+                {
+                    itemsOutOfRange.Add(item);
+                }
+            }
+            var inRangeCount = itemsInRange.Count();
+            var outOfRangeCount = itemsOutOfRange.Count();
+            itemsInRange.AddRange(itemsOutOfRange);
+            Console.WriteLine();
+            Console.WriteLine("Количество элементов в интервале - " + inRangeCount + ", количество элементов вне интервала - " + outOfRangeCount + ":");
+            itemsInRange.ForEach(x => Console.Write(x + " "));
+            Console.ReadLine();
         }
     }
 }
